a743262373263f765f61c5068b4c78f3d9e2f28b
Daly Perez <dalianysperez@gmail.com>
2021-05-11 15:30:22 -0400

    calculate the largest doc

--- src/main.py
+++ src/main.py
@@ -13,34 +13,36 @@ r = random.Random()
 r.seed(99)
 
 def main():
-    # docs, docs_dict = dataset_dict('../dataset/yolanda/corpus/MED.ALL')
-    # queries, queries_dict = dataset_dict('../dataset/yolanda/queries/MED.QRY')
-    # relevances = read_relevances('../dataset/yolanda/relevance/MED.REL')
+    docs, docs_dict = dataset_dict('../dataset/yolanda/corpus/MED.ALL')
+    queries, queries_dict = dataset_dict('../dataset/yolanda/queries/MED.QRY')
+    relevances = read_relevances('../dataset/yolanda/relevance/MED.REL')
 
-    docs_dict, pdocs = read_all('../dataset/jsons/CISI.ALL.json')
-    queries_dict, pqueries = read_qry('../dataset/jsons/CISI.QRY.json')
-    relevances = read_rel('../dataset/jsons/CISI.REL.json', len(pqueries))
+    print((sum([len(preprocess_document(d)) for d in docs]))/len(docs))
 
-    true, false, relpairs = conforms_pairs(relevances, len(docs_dict))
-    print(len(true), len(false), len(relpairs))
+    # docs_dict, pdocs = read_all('../dataset/jsons/CISI.ALL.json')
+    # queries_dict, pqueries = read_qry('../dataset/jsons/CISI.QRY.json')
+    # relevances = read_rel('../dataset/jsons/CISI.REL.json', len(pqueries))
+
+    # true, false, relpairs = conforms_pairs(relevances, len(docs_dict))
+    # print(len(true), len(false), len(relpairs))
     
-    r.shuffle(false)
+    # r.shuffle(false)
 
-    m = len(false) // 10
-    false = false[:m]
-    relpairs = true + false
-    r.shuffle(relpairs)
+    # m = len(false) // 10
+    # false = false[:m]
+    # relpairs = true + false
+    # r.shuffle(relpairs)
 
-    print( len(true), len(false), len(relpairs))
+    # print( len(true), len(false), len(relpairs))
     
-    fd = open('./word2vect/w2vect50.bin')
-    w2v_dict = js.load(fd)
+    # fd = open('./word2vect/w2vect50.bin')
+    # w2v_dict = js.load(fd)
 
-    start = time.time()
-    TrainSimilarity(docs_dict, queries_dict, relpairs , w2v_dict)
-    end = time.time()
+    # start = time.time()
+    # TrainSimilarity(docs_dict, queries_dict, relpairs , w2v_dict)
+    # end = time.time()
 
-    print(end - start)
+    # print(end - start)
 
    
     
